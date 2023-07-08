cc670705b66e8faab8e8f96b9fe10ab8f90e18d9
KeremZeybek <recepkeremzeybek@gmail.com>
2023-07-08 15:01:54 +0300

    data

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,18 @@
-import Picture from "../components/Picture"
-import data from "../helper/data"
-
+import Picture from "../components/Picture";
+import data from "../helper/data";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Homepage = () => {
+    // console.log(data)
   return (
-    
-    <div>
-      <Picture/>
-    </div>
-  )
-}
+    <Container>
+      <h1 className="text-center mt-4 text-success">Albums</h1>
+      <Row className="gap-5">
+        
+      </Row>
+      <Picture />
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
